03a89820aceeca6c615cc01eecbd6823a609eaeb
Flo Mit H <Flogamesg@gmail.com>
2021-06-28 18:44:58 +0200

    - Complete spam.py - Update .gitignore - Create Config

--- src/cog/spam.py
+++ src/cog/spam.py
@@ -1,13 +1,26 @@
 from discord.ext import commands
 
+from bot import config
+
 
 class Spam(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener
+    @commands.Cog.listener()
     async def on_message(self, message):
-        pass
+
+        if message.author.bot:
+            return
+
+        if message.author is self.bot.user:
+            return
+
+        for word in config["spam"]:
+            if word in message.content:
+                await message.add_reaction(config["arch-emoji"])
+                await message.channel.send("Bloeckchengrafik#0420 uses arch btw")
+                return
 
 
 def setup(bot):
